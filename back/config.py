4163afee0697429ab4a72d8566020fc762c8ef92
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-07-11 10:09:22 +0300

    fix mongo URI
    2024-07-11 08:57:30 Traceback (most recent call last):
    2024-07-11 08:57:30   File "/app/app/main.py", line 6, in <module>
    2024-07-11 08:57:30     from app import app
    2024-07-11 08:57:30   File "/app/app/_init_.py", line 7, in <module>
    2024-07-11 08:57:30     client = MongoClient(app.config['MONGO_URI'])
    2024-07-11 08:57:30 KeyError: 'MONGO_URI'

--- back/config.py
+++ back/config.py
@@ -1,7 +1,4 @@
 import os
 
-class Config:
-    MONGO_URI = f"mongodb://{os.environ.get('MONGODB_USERNAME')}:{os.environ.get('MONGODB_PASSWORD')}@{os.environ.get('MONGODB_HOSTNAME')}:27017/{os.environ.get('MONGODB_DATABASE')}"
-    MONGO_DBNAME = os.environ.get('MONGODB_DATABASE')
-
-app_config = Config()
+MONGO_URI = f"mongodb://{os.environ.get('MONGODB_USERNAME')}:{os.environ.get('MONGODB_PASSWORD')}@{os.environ.get('MONGODB_HOSTNAME')}:27017/{os.environ.get('MONGODB_DATABASE')}?authSource=admin"
+MONGO_DBNAME = os.environ.get('MONGODB_DATABASE')
