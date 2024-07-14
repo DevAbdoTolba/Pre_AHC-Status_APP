a4098f42087c6fa4efb267ae65506eff28819c04
MAMDOUHjr <A.Mohamed13207@student.aast.edu>
2024-07-14 21:06:20 +0300

    close #6 fix date parsing in Data class and handle status if it null because user didn't send this

--- back/app/models.py
+++ back/app/models.py
@@ -1,21 +1,28 @@
 from bson import ObjectId
-from datetime import datetime
-from app import db
+from datetime import datetime, date
+
+
 class Data:
     def __init__(self, name, age=None, level=None, msg=None, important=False, status=None, _id=None):
         try:
             self.id = _id if _id else ObjectId()
             self.name = name
-            self.age = age if age else datetime.now()
+            if isinstance(age, str):
+                self.age = datetime.strptime(age, "%d/%m/%Y").date()
+            elif isinstance(age, date):
+                self.age = age
+            else:
+                self.age = datetime.now().date()
+
             self.level = level
             self.msg = msg
             self.important = important
-            self.status = status
+            self.status = status if status is not None else "open"
         except Exception as e:
             raise ValueError(f"Error initializing Data object: {e}")
 
     def to_dict(self):
-        try:
+         try:
             return {
                 "_id": str(self.id),
                 "name": self.name,
@@ -25,7 +32,7 @@ class Data:
                 "important": self.important,
                 "status": self.status
             }
-        except Exception as e:
+         except Exception as e:
             raise ValueError(f"Error converting Data object to dict: {e}")
 
     @classmethod
@@ -34,28 +41,27 @@ class Data:
             name = data.get("name")
             if not name:
                 raise ValueError("Missing required field: name")
-            
+
             age = data.get("age")
             if age:
-                age = datetime.fromisoformat(age)
-            
+                age = datetime.fromisoformat(age).date()
+
             level = data.get("level")
             if level and level not in ("easy", "normal", "hard"):
                 raise ValueError("Invalid value for field: level")
-            
+
             msg = data.get("msg")
             important = data.get("important", False)
-            
+
             status = data.get("status")
             if status and status not in ("open", "close"):
                 raise ValueError("Invalid value for field: status")
-            
+
             _id = data.get("_id")
             if _id:
                 _id = ObjectId(_id)
-            
+
             return cls(name=name, age=age, level=level, msg=msg, important=important, status=status, _id=_id)
         except Exception as e:
             raise ValueError(f"Error creating Data object from dict: {e}")
-    def save(self):
-        db.data.insert_one(self.to_dict())
+
