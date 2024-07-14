5a6fbb15bb0a173c9d2ad4069d5deef7c668182b
S7S <126897441+hassannasserr@users.noreply.github.com>
2024-07-14 19:12:27 +0300

    close #9 Retreive all data from database
    - Implemented a new route '/get_all' with GET method.
    - Added logic to query the database for items where the status is not 'closed'.
    - Converted the query results to a list and returned as JSON response.
    - Included error handling to return exceptions as a string in case of failure.

--- back/app/routes.py
+++ back/app/routes.py
@@ -1,5 +1,17 @@
-from app import app
+from app import app,db 
+from flask import jsonify, request
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+@app.route('/get_all', methods=['GET'])
+def get_all():
+    try:
+        if request.method == 'GET':
+         condition = {"status": {"$ne": "closed"}}
+         data = db.data.find(condition)
+         data_list = list(data)
+         return jsonify(data_list)
+    except Exception as e:
+        return str(e)
