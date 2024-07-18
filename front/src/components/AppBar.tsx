58acc363b74ee06f6ed5064976af79bf2f59c901
hottestmanalive <boodyewis@gmail.com>
2024-07-18 03:59:48 +0300

    Applied small changes:-
    1)removed redundant comments
    
    2)remove extra white-spaces
    
    3)removed unused import { after } from 'node:test';

--- front/src/components/AppBar.tsx
+++ front/src/components/AppBar.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Stack, Button, Typography, Box, Paper } from '@mui/material';
-import { Block } from '@mui/icons-material';
 import VaccinesIcon from '@mui/icons-material/Vaccines';
-import { after } from 'node:test';
 import Link from 'next/link';
-
-
 
 const MyAppBar: React.FC = () => {
   return <>
@@ -19,42 +15,28 @@ const MyAppBar: React.FC = () => {
       border: "0 5px 5px 5px",
       borderStyle: "hidden solid solid solid",
       borderColor: "white",
-      // , "&::after": {
-      //   content: '""', position: "absolute", width: "100%", height: "100%", borderRadius: "15rem"
-      //   , border: "1px solid black", p: "5px", top: "0"
-      // },
-
       background: "linear-gradient(-133deg, #009788 5%, rgba(0,0,0,0.8940388655462185) 50%);",
       display: "flex",
       flexDirection: "row"
       , justifyContent: "center"
     }}>
-
       <Box display="flex" flexDirection="row" justifyContent={"space-between"} width={"100%"}  >
         <Stack direction={"row"}>
           <Typography variant='h3' sx={{
             fontFamily: "fantasy",
             color: "#009788",
-
-
           }}>S</Typography>
           <Typography variant='h3' sx={{
             fontFamily: "fantasy",
             color: "whitesmoke",
-
-
           }}>ick</Typography>
           <Typography variant='h3' sx={{
             fontFamily: "fantasy",
             color: "#009788",
-
-
           }}>M</Typography>
           <Typography variant='h3' sx={{
             fontFamily: "fantasy",
             color: "whitesmoke",
-
-
           }}>an</Typography>
           <VaccinesIcon sx={{
             fontSize: "50px",
@@ -62,7 +44,6 @@ const MyAppBar: React.FC = () => {
           }}></VaccinesIcon>
         </Stack>
         <Stack sx={{
-
           "& .MuiButtonBase-root": {
             color: "white",
             fontWeight: "bold",
@@ -75,15 +56,10 @@ const MyAppBar: React.FC = () => {
             maxHeight: "5ch",
             textAlign: "center",
             "& .MuiTypography-root": { transition: "0.5s letter-spacing,0.1s color", letterSpacing: "5px", textAlign: "center", },
-
-
-
-
             "&:hover ": {
               background: "#eee",
               color: "black",
               "& .MuiTypography-root": {
-
                 letterSpacing: "10px",
                 // fontSize: "20px",              
               }
@@ -96,9 +72,6 @@ const MyAppBar: React.FC = () => {
               }
             }
           }
-
-
-
         }} direction={"row"} gap={10}>
           <Button href='/' LinkComponent={Link} disableRipple><Typography>
             HOME
@@ -109,13 +82,9 @@ const MyAppBar: React.FC = () => {
           <Button href='/admin' LinkComponent={Link} disableRipple><Typography>
             USER
           </Typography></Button>
-
         </Stack>
-
       </Box>
     </Paper >
   </>
-
 };
-
 export default MyAppBar;
