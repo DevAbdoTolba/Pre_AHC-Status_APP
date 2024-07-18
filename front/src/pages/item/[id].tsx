27bf552d74d47b2c1213845bee55afeb95354083
Girgis emad <126956967+GARMA-A@users.noreply.github.com>
2024-07-18 21:57:50 +0300

    Update [id].tsx

--- front/src/pages/item/[id].tsx
+++ front/src/pages/item/[id].tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import ItemView from "@/components/ItemView";
 
 const ItemPage: React.FC = () => {
   return (
     <>
       <>
-        Item Details
+        <ItemView/>
       </>
       
     </>
