e4465ab35315e1ed759395a43e407dfeaddd01b4
Girgis emad <126956967+GARMA-A@users.noreply.github.com>
2024-07-20 23:33:12 +0300

    Enhance AdminDataGrid Component (#42)
    * view Admin Data Grid and  some UX
    
    * remove yarn.lock file from the repo
    
    * Add files via upload
    
    * delete the global yarn file
    
    * remove yern from ignore
    
    * remove yarn file from ignore.git
    
    * return the yarn file to narmal
    
    * view Admin Data Grid and  some UX
    
    * improve the using of MUI and the performance
    
    * finish the view Data Grid And ux Improve

--- front/src/components/AdminDataGrid.tsx
+++ front/src/components/AdminDataGrid.tsx
@@ -2,49 +2,78 @@
 import Box from '@mui/material/Box';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import * as React from 'react';
+import PriorityHighRoundedIcon from "@mui/icons-material/PriorityHighRounded";
+import { green, yellow, red ,blue } from "@mui/material/colors";
+import { Button, Typography } from '@mui/material';
+import Link from 'next/link';
 
-
-
-  
- const columns: GridColDef<(typeof rows)[number]>[] = [
-  { field: "id", headerName: "ID", width: 150 },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-
-  },
-  {
-    field: "status",
-    headerName: "status",
-    width: 150,
-  },
-  {
-    field: "level",
-    headerName: "Level",
-    width: 150,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "date",
-    width: 150,
-    editable: false,
-  },
-  {
-    field: "important",
-    headerName: "important",
-
-    sortable: false,
-    width: 150,
-  },
-  {
-    field: "message",
-    headerName: "message",
-    width: 150,
-  },
-];
+     
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "firstName",headerName: "First name",width: 150,editable: true},
+    { field: "status", headerName: "Status", width: 150 },
+    {
+      field: "level",
+      headerName: "Level",
+      width: 150,
+      renderCell: (params) => {
+        let color;
+        switch (params.value) {
+          case "easy":
+            color = green[500]; // MUI green color
+            break;
+          case "normal":
+            color = yellow[500]; // MUI yellow color
+            break;
+          case "hard":
+            color = red[500]; // MUI red color
+            break;
+        }
+        return (
+          <Typography
+            sx={{ color: color }}
+            variant="body1"
+          >
+            {params.value}
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "date",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "important",
+      headerName: "Important",
+      width: 150,
+      renderCell: (params) =>
+        params.value === "true" ? (
+          <PriorityHighRoundedIcon sx={{ color: "yellow" }} />
+        ) : (
+          <></>
+        ),
+    },
+    { field: "message", headerName: "Message", width: 150 },
+    {
+      field: "view",
+      headerName: "View",
+      width: 150,
+      renderCell: (params) => (
+        <Button
+          component={Link}
+          href={`/item/${params.row.id}`}
+          variant="contained"
+          color="primary"
+        >
+          View
+        </Button>
+      ),
+    },
+  ];
 
 const rows = [
   {
@@ -76,7 +105,7 @@ const rows = [
   },
   {
     id: 4,
-    level: "high",
+    level: "hard",
     firstName: "Arya",
     age: new Date("2000-02-05"),
     important: "false",
@@ -85,7 +114,7 @@ const rows = [
   },
   {
     id: 5,
-    level: "difficult",
+    level: "hard",
     firstName: "Daenerys",
     age: new Date("2000-02-05"),
     important: "true",
@@ -103,7 +132,7 @@ const rows = [
   },
   {
     id: 7,
-    level: null,
+    level: "hard",
     firstName: "Ferrara",
     age: new Date("2000-02-05"),
     important: "ture",
@@ -121,7 +150,7 @@ const rows = [
   },
   {
     id: 9,
-    level: "difficult",
+    level: "hard",
     firstName: "Harvey",
     age: new Date("2000-02-05"),
     important: "false",
@@ -131,9 +160,10 @@ const rows = [
 ];
 
 
-   export default function DataGridDemo() {
+export default function DataGridDemo() {
+  
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -145,9 +175,8 @@ const rows = [
           },
         }}
         pageSizeOptions={[5]}
-        
+        disableRowSelectionOnClick
       />
-      
     </Box>
   );
    };
