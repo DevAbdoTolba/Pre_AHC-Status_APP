e778a6f699818b3f9267fb5e3e092dc3ebbdaf7a
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-07-22 01:01:56 +0300

    update endpoint

--- front/src/pages/api/get/[id].ts
+++ front/src/pages/api/get/[id].ts
@@ -1,21 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
 
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const response = await fetch(`http://localhost:8700/get/${id}`);
       const data = await response.json();
+      if (data?.error) {
+        throw new Error(data.message);
+      }
       // convert _id to id
       data.id = data._id;
       res.status(200).json(data);
     } catch (error) {
-      const data = JSON.parse('{"data":{"id": "66995c281de728837b87ab70","age": "2004-02-25","important": false,"level": "normal","msg": "Hello","name": "John Doe","status": "open"}}');
-      res.status(200).json(data);
+      // const data = JSON.parse('{"data":{"id": "66995c281de728837b87ab70","age": "2004-02-25","important": false,"level": "normal","msg": "Hello","name": "John Doe","status": "open"}}');
+      res.status(500).json({ error: "Error fetching data" });
     }
   } else {
-    res.setHeader('Allow', ['GET']);
+    res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
