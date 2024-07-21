3237d3b8a47f7e53974ff8e11f839e236be34a18
haneenbadr <100076011+haneenbadr@users.noreply.github.com>
2024-07-21 21:26:06 +0300

    Item Viewing by id

--- front/src/pages/item/[id].tsx
+++ front/src/pages/item/[id].tsx
@@ -1,15 +1,35 @@
-import React from "react";
-import ItemView from "@/components/ItemView";
+import * as React from 'react';
+import { GetServerSideProps } from 'next';
+import ItemView from '../../components/ItemView';
 
-const ItemPage: React.FC = () => {
-  return (
-    <>
-      <>
-        <ItemView/>
-      </>
-      
-    </>
-  );
+interface RowData {
+  id: string;
+  name: string;
+  age: string;
+  level: string;
+  message: string;
+  important: boolean;
+  status: string;
+}
+
+interface ItemPageProps {
+  data: RowData[];
+}
+
+const ItemPage: React.FC<ItemPageProps> = ({ data }) => {
+  return <ItemView data={data} />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params!;
+  const response = await fetch(`api/get/${id}`);
+  const data = (await response.json()).data;
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default ItemPage;
